d70f41ee6069024240309e8835d68d45df377650
gence <gence@users.noreply.github.com>
2022-05-04 22:24:52 +0300

    fix locale issue

--- Source/Microsoft.Teams.Apps.DLLookup/ClientApp/src/i18n.ts
+++ Source/Microsoft.Teams.Apps.DLLookup/ClientApp/src/i18n.ts
@@ -15,7 +15,8 @@ i18n
         keySeparator: false, // we do not use keys in form messages.welcome 
         interpolation: {
             escapeValue: false // react already safes from xss
-        }
+        },
+        load: 'currentOnly'
     });
 
 
